5812408b80a56d808f7e7597d87c50b3688e3dcc
Rohit Chandra <rohitchandrapandit@gmail.com>
2025-06-24 19:24:20 +0000

    <!-- From Uiverse.io by akshat-patel28 -->  <div class="form-container"

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,13 +3,9 @@ import { LoginForm } from "@/components/login-form"
 export default function LoginPage() {
   return (
     <div
-      className="relative flex min-h-screen flex-col items-center justify-center bg-cover bg-center"
-      style={{ backgroundImage: "url('https://images.pexels.com/photos/745365/pexels-photo-745365.jpeg')" }}
+      className="flex min-h-screen flex-col items-center justify-center bg-background"
     >
-      <div className="absolute inset-0 bg-black/60" />
-      <div className="relative z-10">
-        <LoginForm />
-      </div>
+      <LoginForm />
     </div>
   )
 }
