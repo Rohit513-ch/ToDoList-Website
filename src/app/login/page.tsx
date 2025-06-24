740aeaabebdc3579617c7f833c51bdb72d87f3e2
Rohit Chandra <rohitchandrapandit@gmail.com>
2025-06-24 19:08:32 +0000

    https://images.pexels.com/photos/745365/pexels-photo-745365.jpeg

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,8 +6,14 @@ export default function LoginPage() {
   return (
     <div className="flex min-h-screen flex-col bg-background">
       <Header />
-      <main className="flex-1 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <LoginForm />
+      <main
+        className="relative flex-1 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-cover bg-center"
+        style={{ backgroundImage: "url('https://images.pexels.com/photos/745365/pexels-photo-745365.jpeg')" }}
+      >
+        <div className="absolute inset-0 bg-black/60" />
+        <div className="relative">
+          <LoginForm />
+        </div>
       </main>
       <Footer />
     </div>
